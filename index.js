fc77dd146dd6bd13fcba8eb434e898921643aff9
andreeaduta2022 <120908253+andreeaduta2022@users.noreply.github.com>
2023-01-01 12:34:10 +0200

    Small view changes in JS

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ function displayForecast(response){
   let forecastHTML=`<div class="row">`; 
   forecast.forEach(function(forecastDay, index){if (index<5) {
     forecastHTML=forecastHTML+ `<div class="col-2">
-    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <div class="weather-forecast-date"><strong>${formatDay(forecastDay.dt)}</strong></div>
     <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="" />
     <div class="weather-forecast-temperatures">
-    <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°C</span>
-    <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°C</span>
+    <span class="weather-forecast-temperature-max">max${Math.round(forecastDay.temp.max)}°C</span>
+    <span class="weather-forecast-temperature-min">min${Math.round(forecastDay.temp.min)}°C</span>
     </div>
     </div>
     </div>
